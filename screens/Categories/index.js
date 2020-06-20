3ff207a50d3572797aabc041b83e36ab8f4d38f8
mandimartins <mandimartins23@gmail.com>
2020-06-20 14:46:41 -0300

    Add gridItem component to category screen

--- screens/Categories/index.js
+++ screens/Categories/index.js
@@ -7,27 +7,25 @@ import {
   View,
 } from 'react-native';
 
+import CategoryGridTile from '../../components/CategoryGridTile';
+
 import { CATEGORIES } from '../../data/dummy-data';
-import Colors from '../../constants/colors';
 
 const Categories = (props) => {
   const renderGridItem = (itemData) => {
     return (
-      <TouchableOpacity
-        style={styles.gridItem}
-        onPress={() =>
+      <CategoryGridTile
+        color={itemData.item.color}
+        title={itemData.item.title}
+        onSelect={() => {
           props.navigation.navigate({
             routeName: 'CategoryMeals',
             params: {
               categoryId: itemData.item.id,
             },
-          })
-        }
-      >
-        <View>
-          <Text>{itemData.item.title}</Text>
-        </View>
-      </TouchableOpacity>
+          });
+        }}
+      />
     );
   };
 
@@ -43,10 +41,6 @@ const Categories = (props) => {
 
 Categories.navigationOptions = {
   headerTitle: 'Meal Categories',
-  headerStyle: {
-    backgroundColor: Colors.primaryColor,
-  },
-  headerTintColor: 'white',
 };
 
 const styles = StyleSheet.create({
@@ -54,11 +48,6 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-  },
-  gridItem: {
-    flex: 1,
-    margin: 15,
-    height: 150,
   },
 });
 export default Categories;
