a762d1fc193ff942fc7272153a3a00b8c09ed72b
mandimartins <mandimartins23@gmail.com>
2020-06-20 13:19:07 -0300

    Pass data between components with route params

--- screens/Categories/index.js
+++ screens/Categories/index.js
@@ -1,18 +1,36 @@
 import React from 'react';
-import { TouchableOpacity, Text, FlatList, StyleSheet } from 'react-native';
+import {
+  TouchableOpacity,
+  Text,
+  FlatList,
+  StyleSheet,
+  View,
+} from 'react-native';
 
 import { CATEGORIES } from '../../data/dummy-data';
 import Colors from '../../constants/colors';
 
-const renderGridItem = (itemData) => {
-  return (
-    <TouchableOpacity style={styles.gridItem}>
-      <Text>{itemData.item.title}</Text>
-    </TouchableOpacity>
-  );
-};
-
 const Categories = (props) => {
+  const renderGridItem = (itemData) => {
+    return (
+      <TouchableOpacity
+        style={styles.gridItem}
+        onPress={() =>
+          props.navigation.navigate({
+            routeName: 'CategoryMeals',
+            params: {
+              categoryId: itemData.item.id,
+            },
+          })
+        }
+      >
+        <View>
+          <Text>{itemData.item.title}</Text>
+        </View>
+      </TouchableOpacity>
+    );
+  };
+
   return (
     <FlatList
       keyExtractor={(item, index) => item.id}
--- screens/Meals/index.js
+++ screens/Meals/index.js
@@ -1,9 +1,34 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, Button, StyleSheet } from 'react-native';
 
-const Meals = () => {
-  return <View />;
+import { CATEGORIES } from '../../data/dummy-data';
+
+const Meals = (props) => {
+  const catId = props.navigation.getParam('categoryId');
+
+  const selectedCategory = CATEGORIES.find((cat) => cat.id === catId);
+
+  return (
+    <View style={styles.screen}>
+      <Text>The Category Meal Screen</Text>
+      <Text>{selectedCategory.title}</Text>
+      <Button
+        title="Go to Details"
+        onPress={() =>
+          props.navigation.navigate({
+            routeName: 'MealsDetails',
+          })
+        }
+      />
+    </View>
+  );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 export default Meals;
