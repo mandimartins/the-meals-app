66da94df02c676c247bc56688ea1e933d67f919a
mandimartins <mandimartins23@gmail.com>
2020-06-20 11:31:46 -0300

    Add react boilerplate

---
+++ screens/Favorites/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const Favorites = () => {
+  return <View />;
+};
+
+const styles = StyleSheet.create({});
+export default Favorites;
