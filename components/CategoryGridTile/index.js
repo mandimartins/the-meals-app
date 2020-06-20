0677866b51244b10f436d6ac17e984c4a5100e69
mandimartins <mandimartins23@gmail.com>
2020-06-20 14:44:18 -0300

    Make gridItem a component

---
+++ components/CategoryGridTile/index.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import {
+  View,
+  TouchableOpacity,
+  TouchableNativeFeedback,
+  Text,
+  StyleSheet,
+  Platform,
+} from 'react-native';
+
+// import { Container } from './styles';
+
+const CategoryGridTile = ({ title, color, onSelect }) => {
+  let TouchableCmp = TouchableOpacity;
+
+  if (Platform.OS === 'android' && Platform.Version >= 21) {
+    TouchableCmp = TouchableNativeFeedback;
+  }
+
+  return (
+    <View style={styles.gridItem}>
+      <TouchableCmp style={{ flex: 1 }} onPress={onSelect}>
+        <View style={{ ...styles.container, ...{ backgroundColor: color } }}>
+          <Text style={styles.title}>{title}</Text>
+        </View>
+      </TouchableCmp>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  gridItem: {
+    flex: 1,
+    margin: 15,
+    height: 150,
+  },
+  container: {
+    flex: 1,
+    borderRadius: 10,
+    elevation: 5,
+    padding: 15,
+    justifyContent: 'flex-end',
+    alignItems: 'flex-end',
+  },
+  title: {
+    fontFamily: 'open-sans-bold',
+    fontSize: 18,
+    textAlign: 'right',
+  },
+});
+
+export default CategoryGridTile;
